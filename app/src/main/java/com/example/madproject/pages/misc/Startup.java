004ec95ef599da4eb55f25484d5301ff4fec1bc1
bmyj0176 <bryong0176@gmail.com>
2025-02-11 20:57:19 +0800

    bug fix

--- app/src/main/java/com/example/madproject/pages/misc/Startup.java
+++ app/src/main/java/com/example/madproject/pages/misc/Startup.java
@@ -2,15 +2,26 @@ package com.example.madproject.pages.misc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.madproject.R;
+import com.example.madproject.pages.Main;
 
 public class Startup extends AppCompatActivity {
 
+    Button justLemmeInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+        justLemmeInButton = findViewById(R.id.justLemmeIn);
+        justLemmeInButton.setOnClickListener(view -> onEnter());
+    }
+    private void onEnter() {
+        Intent login = new Intent(Startup.this, Main.class);
+        startActivity(login);
+        finish();
     }
 }
